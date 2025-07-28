b33eb91eaff8a22b29f5ffee8cc6e6b174062d9e
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-07-28 12:36:49 +0700

    update

--- src/configs/knex.ts
+++ src/configs/knex.ts
@@ -10,7 +10,7 @@ export const knexConfig: Knex.Config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
   },
   migrations: {
     tableName: 'knex_migrations',
